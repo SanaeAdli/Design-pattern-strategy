e39a24d653ba194f4072baead58f4c8f398ffb68
Aitiddir-Hajar <65742385+Aitiddir-Hajar@users.noreply.github.com>
2020-12-06 03:30:08 +0100

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,16 @@
+class InsertionSort implements Strategy {
+
+    public void sort(int[] numbers) {
+       int n = numbers.length;  
+        for (int i = 0; i < n-1; i++) 
+        {  
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (numbers[j] < numbers[min_idx]) 
+                    min_idx = j;  
+            int temp = numbers[min_idx]; 
+            numbers[min_idx] = numbers[i]; 
+            numbers[i] = temp; 
+        } 
+    }
+}
